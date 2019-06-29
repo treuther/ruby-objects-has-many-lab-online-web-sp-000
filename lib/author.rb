83ac4a549e137ab874e7dc67868a19588df678ce
Tracy Reuther <tracyreuther@gmail.com>
2019-06-29 13:25:32 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,33 +1,28 @@
 class Author
-attr_accessor :name, :posts
 
-@@all = []
-@@post_count = 0
+  attr_accessor :name, :posts
 
-def initialize(name)
-  @name = name
-  @@all << self
-end
+  @@post_count = 0
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
 
-def self.all
-  @@all
-end
+  def add_post(post)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
 
-def add_post(post)
-  @posts << post
-  post.author = self
-  @@post_count += 1
-end
+  def add_post_by_title(title)
+    post = Post.new(title)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
 
-def add_post_by_title(title)
-  post = Post.new(title)
-  @posts << post
-  post.author = self
-  @@post_count += 1
-end
-
-def self.post_count
-  @@post_count
-end
-
+  def self.post_count
+    @@post_count
+  end
 end
