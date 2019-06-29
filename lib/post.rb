2ec3621a53bc8a93c197db7cf9e2d5adae5518ea
Tracy Reuther <tracyreuther@gmail.com>
2019-06-29 12:55:02 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,32 +1,16 @@
-# class Post
-# attr_accessor :title, :author
-#
-# def initialize(title)
-#   @title = title
-# end
-#
-# def author_name
-#   if @author == nil
-#     return nil
-#   else
-#     @author.name
-#   end
-# end
-#
-# end
-
 class Post
+attr_accessor :title, :author
 
-  attr_accessor :title, :author
-  def initialize(title)
-    @title = title
-  end
+def initialize(title)
+  @title = title
+end
 
-  def author_name
-    if @author == nil
-      return nil
-    else
-      @author.name
-    end
+def author_name
+  if @author == nil
+    return nil
+  else
+    @author.name
   end
+end
+
 end
