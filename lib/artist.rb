ffcd34a0e073e016b52caa851efd09e9edf61527
Tracy Reuther <tracyreuther@gmail.com>
2019-06-29 12:22:12 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,19 +1,4 @@
 class Artist
-attr_accessor :name
-
-def initialize(name)
-  @name = name
-  all = []
-end
-
-def songs
-  song = Song.new(name)
-  @all << song
-  song.artist = self
-end
-
-def all
-  @all
-end
+attr_accessor :name, :songs
 
 end
