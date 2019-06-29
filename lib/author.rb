9c8751e7f33b63e975cc4ef22b66b7e11f494b54
Tracy Reuther <tracyreuther@gmail.com>
2019-06-29 12:53:25 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,28 +1,58 @@
+# class Author
+# attr_accessor :name, :posts
+# 
+# @@post_count = 0
+# 
+# def initialize(name)
+#   @name = name
+#   @posts = []
+# end
+# 
+# def add_post(post)
+#   @posts << post
+#   post.author = self
+#   @@post_count += 1
+# end
+# 
+# def add_post_by_title(title)
+#   post = Post.new(title)
+#   @posts << post
+#   post.author = self
+#   @@post_count += 1
+# end
+# 
+# def self.post_count
+#   @@post_count
+# end
+# 
+# end
+
+
 class Author
-attr_accessor :name, :posts
 
-@@post_count = 0
+  attr_accessor :name, :posts
 
-def initialize(name)
-  @name = name
-  @posts = []
-end
+  @@post_count = 0
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
 
-def add_post(post)
-  @posts << post
-  post.author = self
-  @@post_count += 1
-end
+  def add_post(post)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
 
-def add_post_by_title(title)
-  post = Post.new(title)
-  @posts << post
-  post.author = self
-  @@post_count += 1
-end
+  def add_post_by_title(title)
+    post = Post.new(title)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
 
-def self.post_count
-  @@post_count
-end
-
+  def self.post_count
+    @@post_count
+  end
 end
