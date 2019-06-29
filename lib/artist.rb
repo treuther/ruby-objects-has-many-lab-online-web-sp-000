28120b9f9f881fc275bc24dfd68bca5aa697fe52
Tracy Reuther <tracyreuther@gmail.com>
2019-06-29 12:27:20 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,6 +14,13 @@ def add_song(song)
   @@song_count += 1
 end
 
+def add_song_by_name(name)
+  song = Song.new(name)
+  @songs << song
+  song.artist = self
+  @@song_count += 1
+end
+
 def self.song_count
   @@song_count
 end
